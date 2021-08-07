cc37c80787c4ea7706b2931c01dcf33e1c31b0f8
Christopher Nafrere <nafrere.christopher@gmail.com>
2021-08-08 00:53:50 +0200

    Suppression du projet de base et ajout d'un header adapter a l'état de l'utilisateur

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state= {connexion:false  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Groupomania</h1>
+                    <nav className="Nav-right">
+                        {this.state.connexion ? 
+                            <Fragment>
+                                <h2>Mon compte</h2>
+                                <h2>Se Déconnecter</h2>
+                            </Fragment> 
+                            : 
+                            <Fragment>
+                                <h2>S'identifier</h2>
+                                <h2>S'inscrire</h2>
+                            </Fragment>
+                        }
+                    </nav>
+                </header>
+            </div>
+        )
+    }
 }
-
-export default App;
