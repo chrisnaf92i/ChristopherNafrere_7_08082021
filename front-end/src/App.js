bcb2de13855263a6ab218aaf8d935cfe879c2346
Christopher Nafrere <nafrere.christopher@gmail.com>
2021-08-08 12:17:16 +0200

    ajout du state enregistrer et de l'affichage conditionnel des formulaire d'authentification

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component, Fragment } from 'react'
+import Signup from './components/auth/signup/Signup'
 import "./App.css"
+import Login from './components/auth/login/Login'
 
 export default class App extends Component {
-    state= {connexion:false  }
+    state= {connexion:false, registered:false  }
+
+    openSignup = () => {this.state.registered = false}
+    openLogin = () => {this.state.registered = true}
+     
     render() {
         return (
             <div className="App">
@@ -16,12 +22,16 @@ export default class App extends Component {
                             </Fragment> 
                             : 
                             <Fragment>
-                                <h2>S'identifier</h2>
-                                <h2>S'inscrire</h2>
+                                <h2 onClick={() => this.setState({registered:true})}>S'identifier</h2>
+                                <h2 onClick={() =>  this.setState({registered:false})}>S'inscrire</h2>
                             </Fragment>
                         }
                     </nav>
                 </header>
+
+                <section>
+                    {this.state.registered ? <Login/> : <Signup/>}
+                </section>
             </div>
         )
     }
