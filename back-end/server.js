eca1cfb88cf41c41e20c8ef21b41335faa94d220
Christopher Nafrere <nafrere.christopher@gmail.com>
2021-08-29 18:25:00 +0200

    création des base du server

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,19 @@
 import express from "express"
+import router from "./router/routes.js"
 
 const app = express ()
 
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(router)
 
 app.listen( PORT, console.log(`lancement du server sur le port ${PORT}`))
 
