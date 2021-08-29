73b714c3d23d5c76f9f91bee54f2c19677fbb979
Christopher Nafrere <nafrere.christopher@gmail.com>
2021-08-29 18:29:24 +0200

    création du formulaire de connexion

--- front-end/src/components/auth/login/Login.js
+++ front-end/src/components/auth/login/Login.js
@@ -2,25 +2,51 @@ import React, { Component } from 'react'
 import "./login.css"
 
 export default class Login extends Component {
+    constructor(props)
+    {
+        super(props)
+
+        this.state = {
+            email:"",
+            password:""
+        }
+    }
+
+    handleChangeEmail = (event) => {
+        this.state.email = event.target.value
+        console.log(this.state.email)
+    }
+
+    handleChangePassword = (event) => {
+        this.state.password = event.target.value
+        console.log(this.state.password)
+    }
+
+    handleSubmit = (event) => 
+    {
+        console.log(JSON.stringify(this.state))
+        fetch("/api/auth/login", {method:"post",headers: {"ContentType":"application/json"}, body:JSON.stringify(this.state)})
+        event.preventDefault();
+    }
     render() {
         return (
-            <div  style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+            <form onSubmit={this.handleSubmit}  style={{width:"100%", display:"flex", flexDirection:"column", alignItems:"center"}}>
                 <div>
                     <label for="email">Email :</label>
                     <br/>
-                    <input type="email" id="email"/>
+                    <input onChange={this.handleChangeEmail} type="email" id="email" required/>
                 </div>
                 <br/>
                 <br/>
                 <div>
                     <label for="password">Mot de passe : </label>
                     <br/>
-                    <input type="password" id="password"/>
+                    <input onChange={this.handleChangePassword} type="password" id="password" required/>
                 </div>
                 <br/>
                 <br/>
-                <button className="btn-connexion">Se connecter</button>
-            </div>
+                <input  type="submit"/>
+            </form>
         )
     }
 }
