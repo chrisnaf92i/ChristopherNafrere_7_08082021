14b1f8c97f8aa30ad06921517cb46532cce304fe
Christopher Nafrere <nafrere.christopher@gmail.com>
2021-08-29 18:29:48 +0200

    création du formulaire d'inscription

--- front-end/src/components/auth/signup/Signup.js
+++ front-end/src/components/auth/signup/Signup.js
@@ -2,58 +2,110 @@ import React, { Component} from 'react'
 import "./signup.css"
 
 export default class Signup extends Component {
+
+    constructor(props)
+    {
+        super(props)
+
+        this.state = {
+            lastName:"",
+            firstName:"",
+            birthday:"",
+            email:"",
+            password:""
+        }
+    }
+
+    handleChangeLastName = (event) => {
+        this.state.lastName = event.target.value
+        console.log(this.state.lastName)
+    }
+
+    handleChangeFirstName = (event) => {
+        this.state.firstName = event.target.value
+        console.log(this.state.firstName)
+    }
+
+
+    handleChangeEmail = (event) =>
+    {
+        this.state.email = event.target.value
+        console.log(this.state.email)
+    }
+
+    handleChangePassword = (event) =>
+    {
+        this.state.password = event.target.value
+        console.log(this.state.password)
+    }
+
+    handleChangeBirthday = (event) => 
+    {
+        this.state.birthday = event.target.value
+        console.log(this.state.birthday)
+    }
+
+    handleSubmit = (event) => {
+
+        fetch("/api/auth/signup", {method:"post",headers:{"Content-Type":"application/json"}, body:JSON.stringify(this.state)})
+        alert("Inscription Réalisé");
+
+        event.preventDefault();
+    }
+
+
+
     render() {
         return (
-            <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
-                <div>
+            <form onSubmit={this.handleSubmit} style={{width:"30%", margin:"auto", display:'flex', flexDirection:"column"}}>
+                <div style={{margin:"25px"}}>
                     <label htmlFor="name">Nom :</label>
                     <br/>
-                    <input type="text" id="name"  placeholder="ex : kirstein"/>
+                    <input type="text" onChange={this.handleChangeLastName} id="name"  placeholder="ex : kirstein"/>
                 </div>
                     
                 <br/>
                 <br/>
 
-                <div>
+                <div style={{margin:"25px"}}>
                     <label htmlFor="first-name">Prénom :</label>
                     <br/>
-                    <input type="text" id="first-name" placeholder="ex : jean"/>
+                    <input onChange={this.handleChangeFirstName}  type="text" id="first-name" placeholder="ex : jean" required/>
                 </div>
                     
                 <br/>
                 <br/>
 
-                <div>
+                <div style={{margin:"25px"}}>
                     <label htmlFor="birthday">Date de naissance :</label>
                     <br/>
-                    <input type="date" id="birthday"/>
+                    <input onChange={this.handleChangeBirthday} type="date" id="birthday" required/>
                 </div>
                     
                 <br/>
                 <br/>
                 
-                <div>
+                <div style={{margin:"25px"}}>
                     <label htmlFor="email">email :</label>
                     <br/>
-                    <input type="email" id="email" placeholder="ex : exemple@mail.fr"/>
+                    <input onChange={this.handleChangeEmail} type="email" id="email" placeholder="ex : exemple@mail.fr" required/>
                 </div>
                     
                 <br/>
                 <br/>
 
 
-                <div>
+                <div style={{margin:"25px"}}>
                     <label htmlFor="password">Mot de passe :</label>
                     <br/>
-                    <input type="password" id="password"/>
+                    <input onChange={this.handleChangePassword} type="password" id="password" required/>
                 </div>
                     
                 <br/>
                 <br/>
 
-                <button className="btn-signup">S'inscrire</button>
-
-            </div>
+                <input type="submit" value="S'inscrire" style={{width: "50%", margin:"auto"}} required/>
+            </form>
         )
     }
 }
