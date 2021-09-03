e6c31e8da2ecfed77d9c3661eec88c86d156f7e5
Christopher Nafrere <nafrere.christopher@gmail.com>
2021-09-03 12:46:29 +0200

    modificationd e la navigation pour l'intégration du rooter

--- front-end/src/components/navigation/LeftNavigation.js
+++ front-end/src/components/navigation/LeftNavigation.js
@@ -5,10 +5,13 @@ export default class LeftNavigation extends Component {
         return (
             <aside style={{paddingRight:"15px", borderRight:"1px solid black", width:"15%"}}>
                 <ul style={{listStyleType:"none"}}>
-                    <li><i className="fas fa-user"></i> Mon compte</li>
-                    <li>Multimedia</li>
-                    <li>News</li>
-                    <li><i className="fas fa-sliders-h"></i> Paramètre</li>
+                    <a href="/user" style={{color:"black", textDecoration:"none" , cursor:"pointer"}}><i className="fas fa-user"></i> Mon compte</a>
+                    <br/>
+                    <br/>
+                    <a href="/post" style={{color:"black", textDecoration:"none" , cursor:"pointer"}}>News</a>
+                    <br/>
+                    <br/>
+                    <a href="/parameter" style={{color:"black", textDecoration:"none" , cursor:"pointer"}}><i className="fas fa-sliders-h"></i> Paramètre</a>
 
                 </ul>
             </aside>
